885d6ca0da391151f30e27f91e8729b2ee5b4194
David Koop <dkoop@umassd.edu>
2017-07-04 02:02:13 -0400

    Just delete first cell to get updated cell
    Still need to figure out how to deal with saved nbs

--- dfkernel/resources/kernel.js
+++ dfkernel/resources/kernel.js
@@ -8,13 +8,21 @@ define(["base/js/namespace",
         var onload = function() {
             // initial cell has already been added
             // need to add uuid to it
-            Jupyter.notebook.get_cells().filter(function (c) {
-            return (c.cell_type == 'code'); }).forEach(function(c) {
-               // make sure we have a uuid
-               c.init_dfnb();
-               // set the input prompt to show uuid
-               c.set_input_prompt();
-            });
+            // get rid of the first cell and reload it
+            Jupyter.notebook.delete_cell(0);
+//            Jupyter.notebook.insert_cell_below('code');
+//            Jupyter.notebook.edit_mode(0);
+//            Jupyter.notebook.get_cells().filter(function (c) {
+//            return (c.cell_type == 'code'); }).forEach(function(c) {
+//               // make sure we have a uuid
+//               // question is whether we have to reset the prototype for the cell...
+//               // probably not receiving the event from code mirror about changed cells...
+//               // have to check this with opening notebooks, too
+//               c.init_dfnb();
+//               // set the input prompt to show uuid
+//               c.set_input_prompt();
+//               c.create_element();
+//            });
 
         };
 
