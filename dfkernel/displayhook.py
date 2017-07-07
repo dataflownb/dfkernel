736a28a407947b18186a0f8296b52dd43422c467
David Koop <dkoop@umassd.edu>
2017-07-07 14:48:59 -0400

    Don’t send uuid back, already sent as execution_count

--- dfkernel/displayhook.py
+++ dfkernel/displayhook.py
@@ -10,7 +10,6 @@ class ZMQShellDisplayHook(ipykernel.displayhook.ZMQShellDisplayHook):
     def write_output_prompt(self):
         """Write the output prompt."""
         self.msg['content']['execution_count'] = self.get_execution_count()
-        self.msg['content']['uuid'] = self.get_execution_count()
 
     @property
     def prompt_count(self):
