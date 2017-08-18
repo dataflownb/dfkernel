e7cd972236fda31a963e1c50e6e120c909c6ada4
David Koop <dkoop@umassd.edu>
2017-08-18 12:02:51 -0400

    Send multiple outputs to notebook for a tuple
    - No need for display
    - More uniform
    - Allows better references to cell outputs

--- dfkernel/displayhook.py
+++ dfkernel/displayhook.py
@@ -2,6 +2,7 @@
 
 import ipykernel.displayhook
 from ipykernel.displayhook import *
+from ipykernel.jsonutil import encode_images
 
 class ZMQShellDisplayHook(ipykernel.displayhook.ZMQShellDisplayHook):
     def get_execution_count(self):
@@ -11,6 +12,61 @@ class ZMQShellDisplayHook(ipykernel.displayhook.ZMQShellDisplayHook):
         """Write the output prompt."""
         self.msg['content']['execution_count'] = self.get_execution_count()
 
+    def write_format_data(self, format_dict, md_dict=None):
+        print("WRITING FORMAT DATA:", format_dict)
+        if 0 in format_dict:
+            # have multiple outputs
+            new_format_dict = {}
+            for i in format_dict:
+                new_format_dict[i] = encode_images(format_dict[i])
+            self.msg['content']['data'] = new_format_dict
+        else:
+            self.msg['content']['data'] = encode_images(format_dict)
+        self.msg['content']['metadata'] = md_dict
+
     @property
     def prompt_count(self):
         return self.shell.uuid
+
+    def __call__(self, result=None):
+        """Printing with history cache management.
+
+        This is invoked everytime the interpreter needs to print, and is
+        activated by setting the variable sys.displayhook to it.
+        """
+        self.check_for_underscore()
+        if result is not None and not self.quiet():
+            self.start_displayhook()
+            self.write_output_prompt()
+            if isinstance(result, tuple):
+                # compute format for each result
+                format_dict = {}
+                md_dict = {}
+                for i, res in enumerate(result):
+                    res_format_dict, res_md_dict = self.compute_format_data(res)
+                    format_dict[i] = res_format_dict
+                    md_dict[i] = res_md_dict
+            else:
+                format_dict, md_dict = self.compute_format_data(result)
+            self.update_user_ns(result)
+            self.fill_exec_result(result)
+            if format_dict:
+                self.write_format_data(format_dict, md_dict)
+                self.log_output(format_dict)
+            self.finish_displayhook()
+
+    def finish_displayhook(self):
+        """Finish up all displayhook activities."""
+        sys.stdout.flush()
+        sys.stderr.flush()
+        if self.msg['content']['data']:
+            if 0 in self.msg['content']['data']:
+                format_data = self.msg['content']['data']
+                md_data = self.msg['content']['data']
+                for i in format_data:
+                    self.msg['content']['data'] = format_data[i]
+                    self.msg['content']['metadata'] = md_data[i]
+                    self.session.send(self.pub_socket, self.msg, ident=self.topic)
+            else:
+                self.session.send(self.pub_socket, self.msg, ident=self.topic)
+        self.msg = None
