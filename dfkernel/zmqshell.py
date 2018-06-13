bbe4129e92a773fa07da104cbe939dcc66f20368
Colin Brown <cbrown12@umassd.edu>
2018-06-13 12:14:28 -0400

    Logic fix for creating LinkedResult

--- dfkernel/zmqshell.py
+++ dfkernel/zmqshell.py
@@ -667,7 +667,7 @@ class ZMQInteractiveShell(ipykernel.zmqshell.ZMQInteractiveShell):
                         append_node = True
                         libs = list(diff)
 
-            if(len(unnamed)+len(vars)+len(libs) <= 1 and len(vars) < 1):
+            if(len(unnamed) <= 1 and len(vars)+len(libs) < 1):
                 create_node = False
 
             if create_node:
