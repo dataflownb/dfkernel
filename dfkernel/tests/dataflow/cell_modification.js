833c6dd61ff10dd3e7d3be0df56c780bd406a6f4
Hieu Ngo (Henry) <ngohieu241@gmail.com>
2018-07-18 11:33:13 -0400

    Remove unnecessary tests

--- dfkernel/tests/dataflow/cell_modification.js
+++ dfkernel/tests/dataflow/cell_modification.js
@@ -14,32 +14,19 @@ casper.notebook_test(function () {
 
     this.wait_for_kernel_ready();
 
-    //By assigning this here we speed up the test slightly by not requiring a later this.then block
-    var uuid = '';
-
     this.evaluate(function () {
         Jupyter.notebook.insert_cell_at_index("code", 0);
         var cell = Jupyter.notebook.get_cell(0);
-        uuid = cell.uuid;
         cell.set_text('c,d = 5,6');
         cell.execute();
     });
 
     this.wait_for_output(0);
 
-    this.then(function () {
-        var outputs = get_outputs(0);
-        this.test.assertEquals(outputs.length, 2, 'cell 0 has the right number of outputs');
-        this.test.assertEquals(outputs[0].data['text/plain'], '5', 'cell 0 produces the correct first output');
-        this.test.assertEquals(outputs[0].metadata.output_tag, 'c', 'cell 0 has the correct first output_tag');
-        this.test.assertEquals(outputs[1].data['text/plain'], '6', 'cell 0 produces the correct second output');
-        this.test.assertEquals(outputs[1].metadata.output_tag, 'd', 'cell 0 has the correct second output_tag');
-    });
 
     this.then(function () {
         this.evaluate(function () {
             var cell = Jupyter.notebook.get_cell(0);
-            uuid = cell.uuid;
             cell.set_text('a,c,d = 4,5,6');
             cell.execute();
         });
@@ -49,13 +36,13 @@ casper.notebook_test(function () {
 
     this.then(function () {
         var outputs = get_outputs(0);
-        this.test.assertEquals(outputs.length, 3, 'cell 0 has the right number of outputs');
-        this.test.assertEquals(outputs[0].data['text/plain'], '4', 'cell 0 produces the correct first output');
-        this.test.assertEquals(outputs[0].metadata.output_tag, 'a', 'cell 0 has the correct first output_tag');
-        this.test.assertEquals(outputs[1].data['text/plain'], '5', 'cell 0 produces the correct second output');
-        this.test.assertEquals(outputs[1].metadata.output_tag, 'c', 'cell 0 has the correct second output_tag');
-        this.test.assertEquals(outputs[2].data['text/plain'], '6', 'cell 0 produces the correct third output');
-        this.test.assertEquals(outputs[2].metadata.output_tag, 'd', 'cell 0 has the correct third output_tag');
+        this.test.assertEquals(outputs.length, 3, 'after modification, cell 0 has the right number of outputs');
+        this.test.assertEquals(outputs[0].data['text/plain'], '4', 'after modification, cell 0 produces the correct first output');
+        this.test.assertEquals(outputs[0].metadata.output_tag, 'a', 'after modification, cell 0 has the correct first output_tag');
+        this.test.assertEquals(outputs[1].data['text/plain'], '5', 'after modification, cell 0 produces the correct second output');
+        this.test.assertEquals(outputs[1].metadata.output_tag, 'c', 'after modification, cell 0 has the correct second output_tag');
+        this.test.assertEquals(outputs[2].data['text/plain'], '6', 'after modification, cell 0 produces the correct third output');
+        this.test.assertEquals(outputs[2].metadata.output_tag, 'd', 'after modification, cell 0 has the correct third output_tag');
     });
 
     this.evaluate(function () {
@@ -66,15 +53,6 @@ casper.notebook_test(function () {
         });
 
     this.wait_for_output(1);
-
-    this.then(function () {
-        var outputs = get_outputs(1);
-        this.test.assertEquals(outputs.length, 2, 'cell 1 has the right number of outputs');
-        this.test.assertEquals(outputs[0].data['text/plain'], '1', 'cell 1 produces the correct first output');
-        this.test.assertEquals(outputs[0].metadata.output_tag, 'e', 'cell 1 has the correct first output_tag');
-        this.test.assertEquals(outputs[1].data['text/plain'], '2', 'cell 1 produces the correct second output');
-        this.test.assertEquals(outputs[1].metadata.output_tag, 'f', 'cell 1 has the correct second output_tag');
-    });
 
     this.then(function () {
         this.evaluate(function () {
@@ -88,9 +66,9 @@ casper.notebook_test(function () {
 
     this.then(function () {
         var outputs = get_outputs(1);
-        this.test.assertEquals(outputs.length, 1, 'cell 1 has the right number of outputs');
-        this.test.assertEquals(outputs[0].data['text/plain'], '(1, 2, 4)', 'cell 1 produces the correct output');
-        this.test.assertEquals(outputs[0].output_type, 'execute_result', 'cell 1 produces the correct type of output');
+        this.test.assertEquals(outputs.length, 1, 'after modification, cell 1 has the right number of outputs');
+        this.test.assertEquals(outputs[0].data['text/plain'], '(1, 2, 4)', 'after modification, cell 1 produces the correct output');
+        this.test.assertEquals(outputs[0].output_type, 'execute_result', 'after modification, cell 1 produces the correct type of output');
 
     });
 
