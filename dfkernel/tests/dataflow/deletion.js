73ed4c8929c0b8a748f6ea47cea6d6b811c35124
Hieu Ngo (Henry) <ngohieu241@gmail.com>
2018-07-18 11:27:22 -0400

    Add more test case and remove unnecessary tests

--- dfkernel/tests/dataflow/deletion.js
+++ dfkernel/tests/dataflow/deletion.js
@@ -1,5 +1,5 @@
 //
-// Test cells with multiple tags
+// Test cells with deletion
 //
 
 casper.notebook_test(function () {
@@ -14,54 +14,26 @@ casper.notebook_test(function () {
 
     this.wait_for_kernel_ready();
 
-    //By assigning this here we speed up the test slightly by not requiring a later this.then block
-    var uuid = '';
-
     this.evaluate(function () {
         Jupyter.notebook.insert_cell_at_index("code", 0);
-        var cell = Jupyter.notebook.get_cell(0);
-        uuid = cell.uuid;
-        cell.set_text('c,d = 5,6');
-        cell.execute();
+        Jupyter.notebook.insert_cell_at_index("code", 1);
+        var cell0 = Jupyter.notebook.get_cell(0);
+        var cell1 = Jupyter.notebook.get_cell(1);
+        cell0.set_text('c,d = 5,6');
+        cell1.set_text('a,b= 3,4');
+        cell0.execute();
+        cell1.execute();
     });
 
     this.wait_for_output(0);
-
-    this.then(function () {
-        var outputs = get_outputs(0);
-        this.test.assertEquals(outputs.length, 2, 'cell 0 has the right number of outputs');
-        this.test.assertEquals(outputs[0].data['text/plain'], '5', 'cell 0 produces the correct first output');
-        this.test.assertEquals(outputs[0].metadata.output_tag, 'c', 'cell 0 has the correct first output_tag');
-        this.test.assertEquals(outputs[1].data['text/plain'], '6', 'cell 0 produces the correct second output');
-        this.test.assertEquals(outputs[1].metadata.output_tag, 'd', 'cell 0 has the correct second output_tag');
-    });
-
-
-    this.then(function() {
-        this.evaluate(function () {
-            Jupyter.notebook.insert_cell_at_index("code", 1);
-            var cell = Jupyter.notebook.get_cell(1);
-            cell.set_text('a,b= 3,4');
-            cell.execute();
-        });
-    });
     this.wait_for_output(1);
-
-    this.then(function () {
-        var outputs = get_outputs(1);
-        this.test.assertEquals(outputs.length, 2, 'cell 1 has the right number of outputs');
-        this.test.assertEquals(outputs[0].data['text/plain'], '3', 'cell 1 produces the correct first output');
-        this.test.assertEquals(outputs[0].metadata.output_tag, 'a', 'cell 1 has the correct first output_tag');
-        this.test.assertEquals(outputs[1].data['text/plain'], '4', 'cell 1 produces the correct second output');
-        this.test.assertEquals(outputs[1].metadata.output_tag, 'b', 'cell 1 has the correct second output_tag');
-    });
 
     this.then(function () {
         this.evaluate(function () {
             Jupyter.notebook.delete_cell(0);
             Jupyter.notebook.insert_cell_at_index("code", 1);
             var cell = Jupyter.notebook.get_cell(1);
-            cell.set_text('c=1');
+            cell.set_text('c,d=1,2');
             cell.execute();
         });
     });
@@ -70,11 +42,37 @@ casper.notebook_test(function () {
 
     this.then(function () {
         var outputs = get_outputs(1);
-        this.test.assertEquals(outputs.length, 1, 'cell 2 has the right number of outputs');
-        this.test.assertEquals(outputs[0].data['text/plain'], '1', 'cell 2 produces the correct output');
-        this.test.assertEquals(outputs[0].metadata.output_tag, 'c', 'cell 2 has the correct first output_tag');
+        this.test.assertEquals(outputs[0].data['text/plain'], '1', 'cell 2 produces the correct output, cell deletion works successfully');
+        this.test.assertEquals(outputs[0].metadata.output_tag, 'c', 'cell 2 has the correct output_tag');
 
     });
 
+    this.evaluate(function () {
+        Jupyter.notebook.insert_cell_at_index("code", 2);
+        var cell = Jupyter.notebook.get_cell(2);
+        cell.set_text('e = 5');
+        cell.execute();
+    });
 
+    this.wait_for_output(2);
+
+    this.then(function () {
+        this.evaluate(function () {
+            Jupyter.notebook.delete_cell(2);
+            Jupyter.notebook.insert_cell_at_index("code", 2);
+            var cell = Jupyter.notebook.get_cell(2);
+            cell.set_text('e=10');
+            cell.execute();
+        });
+    });
+
+    this.wait_for_output(2);
+
+    this.then(function () {
+        var outputs = get_outputs(2);
+        this.test.assertEquals(outputs[0].data['text/plain'], '10', 'cell 2 produces the correct output, cell deletion works successfully');
+        this.test.assertEquals(outputs[0].metadata.output_tag, 'e', 'cell 2 has the correct output_tag');
+
+    });    
 });
+ 
