9dbd134224cfac0c26d2839d4b1a637122fd86e3
Colin Brown <cbrown12@umassd.edu>
2018-08-21 12:00:52 -0400

    Change theme and project name

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -46,7 +46,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = 'dfpy_kernel'
+project = 'Dataflow Kernel'
 copyright = '2018, Dataflow Kernel Developers'
 author = 'Dataflow Kernel Developers'
 
@@ -83,7 +83,8 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+#html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
