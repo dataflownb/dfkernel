39ba8ddc8c04333b253bf9e34c858ca80d6fb9c7
Colin Brown <cbrown12@umassd.edu>
2018-08-22 01:10:29 -0400

    keep cell status for undeleting cell before execution

--- dfkernel/resources/df-notebook/codecell.js
+++ dfkernel/resources/df-notebook/codecell.js
@@ -331,7 +331,11 @@ define([
 
             _super.call(this, data);
             this.code_cached = this.get_text();
-            this.set_icon_status(this.metadata.cell_status || 'edited-new');
+            this.metadata.cell_status = this.metadata.cell_status || 'edited-new';
+            if(this.metadata.cell_status.indexOf('undelete-') !== -1) {
+                this.metadata.cell_status = this.metadata.cell_status.slice(9,);
+            }
+            this.set_icon_status(this.metadata.cell_status);
             this.uuid = uuid;
             this.element.attr('id', this.uuid);
             var aname = $('<a/>');
