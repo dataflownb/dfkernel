d004e5bafb3e1b874078f2f21c93a6ceceba0cd6
Hieu Ngo (Henry) <ngohieu241@gmail.com>
2018-08-24 10:29:05 -0400

    check if cell_status in status_to_saved_map

--- dfkernel/resources/df-notebook/notebook.js
+++ dfkernel/resources/df-notebook/notebook.js
@@ -502,10 +502,10 @@ define([
             });
         //set correct cell_status for saved codecell
         if (cell_data.cell_type === 'code') {
-            if (reverse === false) {
+            if (reverse === false && cell_data.metadata.cell_status in status_to_saved_map) ) {
                 cell_data.metadata.cell_status = status_to_saved_map[cell_data.metadata.cell_status];
             }
-            else {
+            else if (reverse === true && cell_data.metadata.cell_status in revert_status_to_saved_map) {
                 cell_data.metadata.cell_status = revert_status_to_saved_map[cell_data.metadata.cell_status];
             }
         }
