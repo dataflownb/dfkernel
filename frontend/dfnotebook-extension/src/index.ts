f17a457697d073ae93a3ea3d5c478cedd1fddf4c
Colin Brown <colinjbrown@niu.edu>
2022-05-20 10:23:04 -0500

    Change isenabled function to match other version

--- frontend/dfnotebook-extension/src/index.ts
+++ frontend/dfnotebook-extension/src/index.ts
@@ -1555,15 +1555,10 @@ function addCommands(
 //     return widget;
 //   }
 
-  /**
-   * Whether there is an active notebook.
-   */
-  function isEnabled(): boolean {
-    return (
-      tracker.currentWidget !== null &&
-      tracker.currentWidget === shell.currentWidget
-    );
-  }
+
+  const isEnabled = (): boolean => {
+    return Private.isEnabled(shell, tracker as unknown as INotebookTracker);
+  };
 
   const isEnabledAndSingleSelected = (): boolean => {
     return Private.isEnabledAndSingleSelected(shell, tracker as unknown as INotebookTracker);
