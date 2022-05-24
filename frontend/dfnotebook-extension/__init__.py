5b4cffd2a536d8060bf0c8e4c965cc88e296446a
David Koop <dakoop@niu.edu>
2022-05-24 17:00:59 -0500

    Attempted to switch setup.py to reference single "bundle"
    This is currently not working.
    Builds things but then cannot find module.

--- frontend/dfnotebook-extension/__init__.py
+++ frontend/dfnotebook-extension/__init__.py
@@ -1,23 +1,42 @@
 # flake8: noqa: F401
+import json
+from pathlib import Path
+
 from ._version import __version__
 
 
+HERE = Path(__file__).parent.resolve()
+
+
+with (HERE / "labextension" / "package.json").open() as fid:
+    data = json.load(fid)
+
+
 def _jupyter_labextension_paths():
-    return [
-        {
-            "src": "labextensions/@dfnotebook/dfnotebook",
-            "dest": "@dfnotebook/dfnotebook",
-        },
-        {
-            "src": "labextensions/@dfnotebook/dfoutputarea",
-            "dest": "@dfnotebook/dfoutputarea",
-        },
-        {
-            "src": "labextensions/@dfnotebook/dfcells",
-            "dest": "@dfnotebook/dfcells",
-        },
-        {
-            "src": "labextensions/@dfnotebook/dfnotebook-extension",
-            "dest": "@dfnotebook/dfnotebook-extension",
-        },
-    ]
+    return [{
+        "src": "labextension",
+        "dest": data["name"]
+    }]
+
+# from ._version import __version__
+
+
+# def _jupyter_labextension_paths():
+#     return [
+#         {
+#             "src": "labextensions/@dfnotebook/dfnotebook",
+#             "dest": "@dfnotebook/dfnotebook",
+#         },
+#         {
+#             "src": "labextensions/@dfnotebook/dfoutputarea",
+#             "dest": "@dfnotebook/dfoutputarea",
+#         },
+#         {
+#             "src": "labextensions/@dfnotebook/dfcells",
+#             "dest": "@dfnotebook/dfcells",
+#         },
+#         {
+#             "src": "labextensions/@dfnotebook/dfnotebook-extension",
+#             "dest": "@dfnotebook/dfnotebook-extension",
+#         },
+#     ]
--- frontend/dfnotebook-extension/_paths.py
+++
@@ -1,4 +0,0 @@
-from pathlib import Path
-
-LAB_EXTENSION_PATH = Path(__file__).parent / "labextensions"
-MAIN_PACKAGE_PATH = LAB_EXTENSION_PATH / "@dfnotebook" / "dfnotebook-extension"
