13fa2d42d73c73d46db960d0580e3edebfae269b
David Koop <dakoop@niu.edu>
2024-01-19 09:07:04 -0600

    Build updates for 3.0.0 kernel

--- dfkernel/_version.py
+++ dfkernel/_version.py
@@ -4,9 +4,8 @@ store the current version info of the server.
 import re
 from typing import List
 
-# Version string must appear intact for tbump versioning
-version_info = (2, 1, 0)
-__version__ = '.'.join(map(str, version_info))
+# Version string must appear intact for hatch versioning
+__version__ = "3.0.0a1"
 
 # Build up version_info tuple for backwards compatibility
 pattern = r"(?P<major>\d+).(?P<minor>\d+).(?P<patch>\d+)(?P<rest>.*)"
@@ -18,4 +17,4 @@ if match["rest"]:
 version_info = tuple(parts)
 
 kernel_protocol_version_info = (5, 3)
-kernel_protocol_version = "%s.%s" % kernel_protocol_version_info
+kernel_protocol_version = "{}.{}".format(*kernel_protocol_version_info)
